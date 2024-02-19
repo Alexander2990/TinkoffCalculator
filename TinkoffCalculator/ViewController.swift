289a4df27f9fae0207f235bec41e4b89be8d10b8
Alexander <Alexander29_90@icloud.com>
2024-02-19 21:35:20 +0500

    Redesigned the button placement

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+class ViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.currentTitle else { return }
         
         print(buttonText)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Та-дам")
     }
     
 }

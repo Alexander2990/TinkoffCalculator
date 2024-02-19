150836b2674802918ddfd803eee6e359553acf8f
Alexander <Alexander29_90@icloud.com>
2024-02-19 20:36:46 +0500

    Displaying the result by clicking a button

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        
+        print(buttonText)
+    }
+    
 }
 
